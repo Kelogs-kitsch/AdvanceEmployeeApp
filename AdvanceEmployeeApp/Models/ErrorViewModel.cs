c083d84f495bd18b5af4e8b354791dce1772a48e
Mang Kelogs <Mang Kelogs@MANG_KELOGS>
2025-11-23 19:21:22 +0800

    Initial Commit

---
+++ AdvanceEmployeeApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvanceEmployeeApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
